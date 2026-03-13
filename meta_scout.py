e176d020e170768b1c2d3bee01a5c4ae51f55891
Bonje Manza <bonje.alexander.manza@gmail.com>
2026-03-13 23:32:20 +0800

    fix: Update default API endpoint to correct source URL

--- meta_scout.py
+++ meta_scout.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 import pandas as pd
 import requests
 
-DEFAULT_ENDPOINT = "https://api.gms.moontontech.com/api/gms/source/2669606/2756569"
+DEFAULT_ENDPOINT = "https://api.gms.moontontech.com/api/gms/source/2669606/2756568"
 APPROVED_HOSTS = {"api.gms.moontontech.com", "www.mobilelegends.com", "mobilelegends.com"}
 REQUIRED_API_TOKEN_ENV = "MLBB_API_AUTHORIZATION"
 API_ENDPOINT_ENV = "MLBB_META_API_URL"
